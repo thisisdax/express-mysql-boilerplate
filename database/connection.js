96a923cd3fb8efff34af6de889b57ee56ac186d9
Dax Tan <this.is.dax@gmail.com>
2019-09-15 20:41:40 +0800

    Added ExpressJS and MySQL connection.

---
+++ database/connection.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql');
+const db_config = require('../config.json');
+
+let connection;
+const env = process.env.NODE_ENV || 'development';
+
+makeConnection = () => {
+    connection = mysql.createConnection(
+        env === "development" ? db_config.dev : process.env.db_config,
+    );
+    console.log('Attempting connection to database...')
+    connection.connect((err) => {
+        if(err) {
+            console.log('Error when connecting to database: ', err);
+            setTimeout(makeConnection, 5000);
+        } else {
+            console.log('Connected to database successfully')
+        }
+    });
+    connection.on('error', (err) => {
+        console.log('Error on connection: ', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.log(`Database connection lost.`)
+            makeConnection();
+        } else {
+            throw err;
+        }
+    });
+}
+
+makeConnection();
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./database/connection');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// server status
+app.get('/health', (req, res) => {
+    res.send('Server is healthy');
+})
+
+app.get('/home', (req, res) => {
+    console.log('hehe');
+    res.send('Home');
+})
+
+// TODO: authentication for extra marks
+
+// TODO: setup routes after designing tables
+
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`);
+});
